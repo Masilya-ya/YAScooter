bbfe9bd0a8217fbe6e471f1e1ec760085a63f5c2
masilya-ya <masilya@mail.ru>
2025-03-14 10:41:00 +0300

    Deleted file .idea and deleted /url

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://a85d81cf-6c2b-4c67-88ec-664c49b62730.serverhub.praktikum-services.ru/" # url 
+URL_SERVICE = "https:/a85d81cf-6c2b-4c67-88ec-664c49b62730.serverhub.praktikum-services.ru/" # url
 CREATE_ZAKAZ = "/api/v1/orders" # ручка создание заказа
 GET_ZAKAZ = "/v1/orders/t=" # ручка получение 
